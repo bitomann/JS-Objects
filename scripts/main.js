a8d97a85a30d24e0f2f60b75ac12f677be09f820
Bito Mann <50162498+bitomann@users.noreply.github.com>
2019-10-12 14:28:35 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,15 +19,6 @@ const Dad = {
     gender: "male"
 }
 
-
-
-
-
-
-
-
-
-
 const Mom = {
     name: "Angela",
     age: 36,
@@ -54,8 +45,6 @@ console.log(`The wardrobe Height is ${wardrobe.height}.`, `The wardrobe Manufact
 // Square Bracket Notation
 // Lightning Exercises 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
 
-// Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
-
 const empireStateBuilding = {
     stories: 103,
     height: 1453,
@@ -70,21 +59,22 @@ const empireStateBuilding = {
 }
 console.log(`The dimensions of The Empire State Building are as follows: It has ${empireStateBuilding.stories} stories,` ,`It is ${empireStateBuilding.height} tall,`, `has ${empireStateBuilding.squareFeet} sqr ft,`, `${empireStateBuilding.eastWestLength} feet to the W and E,`, `${empireStateBuilding.northSouthLength} to the N and S.`)
 
+// Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
+
 const empireStateBuildingRemaining5 = {
     address: "350 Fifth Avenue, Manhattan, New York 10118",
     constructionDate: 1931,
     cost: 40948900,
     owner: "Empire State Realty Trust",
     architect: "Shreve, Lamb & Harmon"
-}
-console.log(empireStateBuildingRemaining5[constructionDate], empireStateBuildingRemaining5[cost], empireStateBuildingRemaining5[owner], empireStateBuildingRemaining5[architect])
+ }
 
+ const keyToLookUp = ["address", "constructionDate", "cost", "owner", "architect"]
+ console.log(keyToLookUp)
 
 
 // Arrays as Values
 // Lightning Exercise 1: Output the names of the part-time instructors followed by the names of the full-time instructors in the console.
-
-// Lightning Exercise 2: Output only Andy and Zoe in the console.
 
 const nashvilleSoftwareSchool = {
     founded: 2012,
@@ -96,4 +86,10 @@ const nashvilleSoftwareSchool = {
     address: "500 Interstate Blvd. S"
 }
 
+const partTime = nashvilleSoftwareSchool.instructors.partTime
+console.log(partTime)
 
+// Lightning Exercise 2: Output only Andy and Zoe in the console.
+
+const instructors = ["Andy", "Zoe"]
+console.log(instructors)
